b1034f8898d37a4dbbefde4f24f6025cefa6d507
svenskaskolan <swedishlandlord@hotmail.com>
2021-08-05 22:44:20 +0200

    begin adding components and styling

---
+++ src/components/AddBoxForm.js
@@ -0,0 +1,15 @@
+
+const AddBoxForm = () => {
+    return (
+    <div className="AddBoxForm">
+        <div>
+            <input className="input" placeholder="Name" type="text" />
+        </div>
+        <div>
+            <input className="input" placeholder="Weight" type="text" />
+        </div>
+    </div>
+    )
+}
+
+export default AddBoxForm;
